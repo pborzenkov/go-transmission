8fbafe28fcdb70c6ec66a2d769010146d7a4fc02
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2020-04-23 12:38:48 +0300

    transmission: add helper function to create optional values
    Optional JSON values are represented in Go as pointers to actual values,
    so that it's possible to set zero values as well. To make it more
    convenient to create such values, add helper functions.
    
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ transmission/optional.go
@@ -0,0 +1,53 @@
+package transmission
+
+import (
+	"time"
+)
+
+// OptInt is a helper routine that allocates new int to store v and returns a
+// pointer to it.
+func OptInt(v int) *int {
+	return &v
+}
+
+// OptInt64 is a helper routine that allocates new int64 to store v and returns
+// a pointer to it.
+func OptInt64(v int64) *int64 {
+	return &v
+}
+
+// OptFloat64 is a helper routine that allocates new float64 to store v and
+// returns a pointer to it.
+func OptFloat64(v float64) *float64 {
+	return &v
+}
+
+// OptBool is a helper routine that allocates new bool to store v and return a
+// pointer to it.
+func OptBool(v bool) *bool {
+	return &v
+}
+
+// OptString is a helper routine that allocates new string to store v and
+// returns a pointer to it.
+func OptString(v string) *string {
+	return &v
+}
+
+// OptDuration is a helper routine that allocates new time.Duration to store v
+// and return a pointer to it.
+func OptDuration(v time.Duration) *time.Duration {
+	return &v
+}
+
+// OptWeekday is a helper routine that allocates new Weekday to store v and
+// returns a pointer to it.
+func OptWeekday(v Weekday) *Weekday {
+	return &v
+}
+
+// OptEncryption is a helper routine that allocates new Encryption to store v
+// and returns a pointer to it.
+func OptEncryption(v Encryption) *Encryption {
+	return &v
+}
