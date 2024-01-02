206b23efce1db81bd2733b3e0674695fae56e684
Pavel Borzenkov <pavel@borzenkov.net>
2024-01-02 14:58:54 +0100

    Move to flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "go-transmission";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.revive
+          pkgs.go-tools
+        ];
+      };
+    });
+}
